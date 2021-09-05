39401733b998bcfab8ce523c25ea1ac60c051897
sakarice <41548156+sakarice@users.noreply.github.com>
2021-09-05 14:35:27 +0900

    リファクタリング

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -94,29 +94,6 @@
     </div>
  
 
-    {{-- @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        {{ __('You are logged in!') }}
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    @endsection --}}
-
-{{-- <script src="{{ asset('js/search_result.js') }}"></script> --}}
 <script src="{{ mix('/js/app.js') }}"></script>
 
     
