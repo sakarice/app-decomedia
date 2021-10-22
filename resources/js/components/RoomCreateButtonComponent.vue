7e95de2ed99b7104d2a14d5a2255a22c5946ee27
sakarice <41548156+sakarice@users.noreply.github.com>
2021-10-22 20:14:21 +0900

    room作成画面内の「作成」ボタンをアイコンに変更

--- resources/js/components/RoomCreateButtonComponent.vue
+++ resources/js/components/RoomCreateButtonComponent.vue
@@ -1,9 +1,10 @@
 <template>
 
-    <div class="action-button-wrapper">
-      <button class="action-button room-create-button" @click="createRoom">
-        作成
-      </button>
+    <div class="room-create-trigger-wrapper">
+      <div class="room-create-trigger" @click="createRoom">
+        <i class="fas fa-check fa-2x create-icon"></i>
+      </div>
+      <span class="create-trigger-subtitle">作成</span>
       <p v-show="message != ''">
         {{message}}
       </p>
@@ -38,5 +39,30 @@
 
 /* ボタン共通のCSS。対象にはaction-buttonクラスを付けること */
 @import "../../css/button.css";
+
+.room-create-trigger-wrapper {
+  display: flex;
+  align-items: center;
+  margin: 0 10px;
+  color: white;
+}
+.room-create-trigger {
+  background-color: greenyellow;
+  padding: 9px 10px;
+  border-radius: 50%;
+  box-shadow: 1px 1px 3px green;
+  transition: 0.13s;
+}
+.room-create-trigger:hover {
+  background-color: aquamarine;
+  color: black;
+  cursor: pointer;
+}
+
+.create-trigger-subtitle {
+  font-size:12px;
+  color: grey;
+  margin-left: 7px;
+}
 
 </style>
