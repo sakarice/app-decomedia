1e3bee06d507a1317e9fb62c621a1cbbcdab3630
sakarice <41548156+sakarice@users.noreply.github.com>
2021-11-06 12:39:55 +0900

    関数などのお試し用にTestクラスを作成

--- app/Lib/Common/StringProcessing.php
+++ app/Lib/Common/StringProcessing.php
@@ -5,75 +5,27 @@ namespace App\Lib\Common;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-use App\Lib\ImgUtil;
-use App\Lib\AudioUtil;
-use App\Lib\StoreFileInS3;
-use App\Models\User;
-use App\Models\DefaultBgm;
-use App\Models\DefaultImg;
-use App\Models\UserOwnBgm;
-use App\Models\UserOwnImg;
-use App\Models\Room;
-use App\Models\RoomBgm;
-use App\Models\RoomImg;
-use App\Models\Roomlist;
-use App\Models\RoomRoomlist;
-
-use Storage;
 
 class StringProcessing
 {
     // 拡張子を除いたファイル名を取得する
-    // ※実態は↓の関数を複合した処理
     public static function getFilenameExceptExt($filename){
-      // ファイル名から拡張子を抽出する
-      // ※実際にはファイル名末尾に一番近い「.」より後を抽出しているだけなので、
-      //   .より後が拡張子ではない可能性もある。↓のcheckContentsExt関数でチェックすること
-      function getExt($filename){
-        $pos = strrpos($filename, '.');
-        if($pos !== false){
-          \Log::info(mb_substr($filename, $pos-1));
-          return mb_substr($filename, $pos-1);
-        } else {
-          return '';
-        }
-      }
+      // 正規表現チェックのためにファイル名を小文字に整形
+      $lowerFilename = strtolower($filename);
 
-      // ファイル名末尾の拡張子を削除する
-      // ※実際にはファイル名末尾に一番近い「.」より前を抽出しているだけなので、
-      //   .より後が拡張子ではない可能性もある。↓のcheckContentsExt関数でチェックすること
-      function delExt($filename) {
-        $pos = strrpos($filename, '.');
-        if($pos !== false){
-          return mb_substr($filename, 0, $pos-2);
-        } else {
-          return '';
-        }
-      }
+      // 削除対象の拡張子を定義
+      $imgExt = '\.jpg$|\.jpeg$|\.png$|\.gif$'; // 画像ファイルの拡張子を定義
+      $audioExt = '\.wave$|\.aiff$|\.mp3$|\.aac$|\.flac$'; // 音楽ファイルの拡張子を定義
+      $movieExt = '\.mp4$|\.avi$|\.mov$|\.flv$'; // 動画ファイルの拡張子を定義
+      $targetExt = '/'.$imgExt.'|'.$audioExt.'|'.$movieExt.'/i'; // 拡張子をまとめて検索用の正規表現にする
       
-      // 引き数で渡した拡張子が画像、音楽、動画の拡張子であるかをチェックする
-      function checkContentsExt($ext){
-        // チェック対象拡張子を配列で定義。
-        $imgExt = array('jpg', 'jpeg', 'png', 'gif');
-        $audioExt = array('mp3', 'wav', 'aac', 'flac', 'aiff');
-        $movieExt = array('mp4', 'avi', 'mpeg', 'wmv');
-        $contentsExt = array_merge($imgExt, $audioExt, $movieExt);
+      // ヒットした拡張子を削除するために、変換先文字を空に
+      $replace = ''; 
 
-        // チェック用に引き数の拡張子文字列を小文字に整形
-        $lowerExt = strtolower($ext);
-
-        // チェック処理
-        $isContentsExt = in_array($lowerExt, $contentsExt);
-
-        return $isContentsExt;
-      }
-
-
-      if(checkContentsExt(getExt($filename))){
-        return delExt($filename);
-      } else {// 拡張子が無い、もしくはチェック対象拡張子でない場合は、ファイル名をそのまま返す
-        return $filename;
-      }
+      // チェック処理
+      $replacedFilename = preg_replace($targetExt, $replace, $lowerFilename);
+      \Log::info('ファイル名の拡張子削除処理結果：'.$filename." ⇒ ".$replacedFilename);
+      return $replacedFilename;
 
     }
 
