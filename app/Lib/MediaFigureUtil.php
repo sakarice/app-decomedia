5a5b39954ed1363efca6771cd6c3e3f9c42ff209
sakarice <41548156+sakarice@users.noreply.github.com>
2021-12-09 13:05:28 +0900

    【改修】カラム名とリクエストのプロパティ名の対応を関数から参照できるようメンバ変数に定義

--- app/Lib/MediaFigureUtil.php
+++ app/Lib/MediaFigureUtil.php
@@ -12,33 +12,34 @@ use App\Models\MediaFigure;
 class MediaFigureUtil
 {
 
+  // テーブルのカラムとrequestのプロパティ名の対応を連想配列に定義
+  private static $DB_REQ_RELATIONS = array(
+    'media_id' => 'media_id',
+    'type' => 'type',
+    'user_selected_item_group_no' => 'groupNo',
+    'left' => 'left',
+    'top' => 'top',
+    'width' => 'width',
+    'height' => 'height',
+    'degree' => 'degree',
+    'global_alpha' => 'globalAlpha',
+    'layer' => 'layer',
+    'is_draw_fill' => 'isDrawFill',
+    'fill_color' => 'fillColor',
+    'is_draw_stroke' => 'isDrawStroke',
+    'stroke_color' => 'strokeColor',
+  );
+
   // 3.store // Media画像情報をDBに保存
   public static function saveMediaFigureData($media_id, $request){
     $req_media_figures = $request->figures;
     foreach($req_media_figures as $index => $req_media_figure){
       MediaFigureUtil::saveMediaFigureRecord($media_id, $req_media_figure);
-      // $mediaFigure = new MediaFigure();
-      // $mediaFigure->media_id = $media_id;
-      // $mediaFigure->type = $req_media_figure['type'];
-      // $mediaFigure->user_selected_item_group_no = $req_media_figure['groupNo'];
-      // $mediaFigure->left = $req_media_figure['left'];
-      // $mediaFigure->top = $req_media_figure['top'];
-      // $mediaFigure->width = $req_media_figure['width'];
-      // $mediaFigure->height = $req_media_figure['height'];
-      // $mediaFigure->degree = $req_media_figure['degree'];
-      // $mediaFigure->global_alpha = $req_media_figure['globalAlpha'];
-      // $mediaFigure->layer = $req_media_figure['layer'];
-      // $mediaFigure->is_draw_fill = $req_media_figure['isDrawFill'];
-      // $mediaFigure->fill_color = $req_media_figure['fillColor'];
-      // $mediaFigure->is_draw_stroke = $req_media_figure['isDrawStroke'];
-      // $mediaFigure->stroke_color = $req_media_figure['strokeColor'];
-      // $mediaFigure->save();
     }
   }
 
 
   public static function saveMediaFigureRecord($media_id, $media_figure_data){
-    \Log::info($media_figure_data);
     $mediaFigure = new MediaFigure();
     $model = $mediaFigure->create([
       'media_id' => $media_id,
@@ -59,46 +60,14 @@ class MediaFigureUtil
     return $model;
   }
 
-  public static function updateMediaFigureRecord($media_id, $media_figure_datas){
-    $media_figures = MediaFigure::where('media_id', $media_id)->get();
-    foreach($media_figure_datas as $index => $media_figure_data){
-      \Log::info($media_figure_data);
-      $media_figures[$index]->media_id = $media_id;
-      $media_figures[$index]->type = $media_figure_data['type'];
-      $media_figures[$index]->user_selected_item_group_no = $media_figure_data['groupNo'];
-      $media_figures[$index]->left = $media_figure_data['left'];
-      $media_figures[$index]->top = $media_figure_data['top'];
-      $media_figures[$index]->width = $media_figure_data['width'];
-      $media_figures[$index]->height = $media_figure_data['height'];
-      $media_figures[$index]->degree = $media_figure_data['degree'];
-      $media_figures[$index]->global_alpha = $media_figure_data['globalAlpha'];
-      $media_figures[$index]->layer = $media_figure_data['layer'];
-      $media_figures[$index]->is_draw_fill = $media_figure_data['isDrawFill'];
-      $media_figures[$index]->fill_color = $media_figure_data['fillColor'];
-      $media_figures[$index]->is_draw_stroke = $media_figure_data['isDrawStroke'];
-      $media_figures[$index]->stroke_color = $media_figure_data['strokeColor'];
-      $media_figures[$index]->save();
+  public static function updateMediaFigureRecord($media_id, $req_datas){
+    $target_records = MediaFigure::where('media_id', $media_id)->get();
+    foreach($req_datas as $index => $req_data){
+      foreach(self::$DB_REQ_RELATIONS as $column_name => $property_name){        
+        $target_records[$index][$column_name] = $req_data[$property_name];
+      }
+      $target_records[$index]->save();
     }
-    // $model = $media_figures->create([
-    //   'media_id' => $media_id,
-    //   'type' => $media_figure_data['type'],
-    //   // 'user_selected_item_group_no' => $media_figure_data['groupNo'],
-    //   'user_selected_item_group_no' => NULL,
-    //   'left' => $media_figure_data['left'],
-    //   'top' => $media_figure_data['top'],
-    //   'width' => $media_figure_data['width'],
-    //   'height' => $media_figure_data['height'],
-    //   // 'degree' => $media_figure_data['degree'],
-    //   'degree' => 0,
-    //   'global_alpha' => $media_figure_data['globalAlpha'],
-    //   'layer' => $media_figure_data['layer'],
-    //   // 'is_draw_fill' => $media_figure_data['isDrawFill'],
-    //   'is_draw_fill' => true,
-    //   'fill_color' => $media_figure_data['fillColor'],
-    //   // 'is_draw_stroke' => $media_figure_data['isDrawStroke'],
-    //   'is_draw_stroke' => true,
-    //   'stroke_color' => $media_figure_data['strokeColor'],
-    // ]);
   }
 
 
