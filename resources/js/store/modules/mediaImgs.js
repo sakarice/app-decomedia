c3b5ddee8930bcccfbea0611fe05b004303ca8cb
sakarice <41548156+sakarice@users.noreply.github.com>
2021-12-10 17:50:35 +0900

    【製造】メディア画像を複数作成するため、storeを作成

---
+++ resources/js/store/modules/mediaImgs.js
@@ -0,0 +1,42 @@
+const mediaImg = {
+  namespaced : true,
+
+  state : {
+    // DBから取得したデータで下記mediaImgが初期化がされたか。
+    isInitialized : false,
+    // メディア画像情報。show,editモードでは始めにDBのデータで初期化される。
+    mediaImgs : [],
+    // mediaImg : {
+    //   type : 0,
+    //   id : 0,
+    //   url : "",
+    //   width : 500,
+    //   height : 500,
+    //   opacity : 1,
+    //   layer : 0,
+    // },
+    targetObjectIndex : 0,
+  },
+  getters : {
+    getIsInitializedImg : function(state){ return state.isInitialized; },
+    getMediaImgs : function(state){ return state.mediaImgs; },
+    getMediaImg : function(state){ return state.mediaImgs[state.targetObjectIndex]; },
+  },
+  mutations : {
+    deleteMediaImgsObjectItem(state, payload){
+      state.mediaImgs.splice(payload,1) // Imgを1つ削除
+    },
+    addMediaImgsObjectItem(state, payload){
+      state.mediaImgs.push(payload);
+    },
+    updateIsInitializedImgs(state,payload){state.isInitialized = payload},
+    updateMediaImgsObjectItem(state, {index, key, value}){state.mediaImgs[index][key] = value;},
+    setTargetObjectIndex(state, payload){ state.targetObjectIndex = payload },
+
+  },
+  actions : {}
+
+}
+
+export default mediaImgs;
+
