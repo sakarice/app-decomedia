5c7b39dbd10cacc5e841854f73618738a5a1af77
sakarice <41548156+sakarice@users.noreply.github.com>
2022-01-07 14:57:46 +0900

    【改修】日本語以外のフォントファミリー一覧から日本語フォントを除外

--- public/js/app.js
+++ public/js/app.js
@@ -14272,64 +14272,6 @@ var fontFamilyList = {
     }, {
       name: "",
       value: '""'
-    }, {
-      name: "",
-      value: '""'
-    }, {
-      name: "",
-      value: '""'
-    }, // 日本語
-    {
-      name: "Noto Sans Japanese",
-      value: '"Noto Sans JP",sans-serif'
-    }, {
-      name: "Noto Serif Japanese",
-      value: '"Noto Serif JP",serif'
-    }, {
-      name: "Sawarabi Mincho",
-      value: '"Sawarabi Mincho",sans-serif'
-    }, {
-      name: "Mochiy Pop P One",
-      value: '"Mochiy Pop P One",sans-serif'
-    }, {
-      name: "Yuji Mai",
-      value: '"Yuji Mai", serif'
-    }, {
-      name: "Zen Antique",
-      value: '"Zen Antique",serif'
-    }, {
-      name: "Reggae One",
-      value: '"Reggae One", cursive'
-    }, {
-      name: "Rampart One",
-      value: '"Rampart One", cursive'
-    }, {
-      name: "DotGothic16",
-      value: '"DotGothic16", sans-serif'
-    }, {
-      name: "",
-      value: '""'
-    }, {
-      name: "YuMincho",
-      value: '"Yu Mincho", "YuMincho"'
-    }, {
-      name: "Hiragino Sans",
-      value: '"Hiragino Sans"'
-    }, {
-      name: "Kosugi",
-      value: '"Kosugi"'
-    }, {
-      name: "Hannari",
-      value: '"Hannari"'
-    }, {
-      name: "Nikukyu",
-      value: '"Nikukyu"'
-    }, {
-      name: "Nico Moji",
-      value: '"Nico Moji"'
-    }, {
-      name: "",
-      value: '""'
     }]
   },
   getters: {
--- resources/js/store/modules/fontFamilyList.js
+++ resources/js/store/modules/fontFamilyList.js
@@ -25,28 +25,7 @@ const fontFamilyList = {
       {name:"IM Fell DW Pica SC", value:'"IM Fell DW Pica SC"'},
       {name:"Londrina Shadow", value:'"Londrina Shadow"'},
       {name:"", value:'""'},
-      {name:"", value:'""'},
-      {name:"", value:'""'},
 
-      // 日本語
-      {name:"Noto Sans Japanese", value:'"Noto Sans JP",sans-serif'},
-      {name:"Noto Serif Japanese", value:'"Noto Serif JP",serif'},
-      {name:"Sawarabi Mincho", value:'"Sawarabi Mincho",sans-serif'},
-      {name:"Mochiy Pop P One", value:'"Mochiy Pop P One",sans-serif'},
-      {name:"Yuji Mai", value:'"Yuji Mai", serif'},
-      {name:"Zen Antique", value:'"Zen Antique",serif'},
-      {name:"Reggae One", value:'"Reggae One", cursive'},
-      {name:"Rampart One", value:'"Rampart One", cursive'},
-      {name:"DotGothic16", value:'"DotGothic16", sans-serif'},
-      {name:"", value:'""'},
-
-      {name:"YuMincho", value:'"Yu Mincho", "YuMincho"'},
-      {name:"Hiragino Sans", value:'"Hiragino Sans"'},
-      {name:"Kosugi", value:'"Kosugi"'},
-      {name:"Hannari", value:'"Hannari"'},
-      {name:"Nikukyu", value:'"Nikukyu"'},
-      {name:"Nico Moji", value:'"Nico Moji"'},
-      {name:"", value:'""'},
     ]
   },
   getters : {
