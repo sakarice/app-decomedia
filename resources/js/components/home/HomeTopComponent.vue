bda42b82de809c47b708bfd54404ac84816975ac
sakarice <41548156+sakarice@users.noreply.github.com>
2022-02-07 17:15:23 +0900

    【改修】ホーム画面トップのキャッチコピーやボタンの文言を変更

--- resources/js/components/home/HomeTopComponent.vue
+++ resources/js/components/home/HomeTopComponent.vue
@@ -3,38 +3,28 @@
         <!-- {{-- タイトルコピー --}} -->
         <div class="top-message-wrapper">
             <h2 class="top-message">
-                <span>創作に手が出せなかった</span><br>
-                <span>あなたへ</span>
+                <span>創作に手が出せなかったあなたへ</span><br>
             </h2>
+            <!-- <h3 class="sub-catch-copy">
+                <span>画像や音楽を「選んで配置する」だけ。</span><br>
+                <span>1分でできるコンテンツ制作。</span><br>
+            </h3> -->
             <p class="sub-message">
-                0からコンテンツを作るのではなく、<br>
-                既にある画像や音楽、動画を組み合わせて<br>
-                メディアとして公開できるサービスです<br><br>
-                アイデアを形にするためのスキル・時間の<br>
-                ハードルを少しでも下げ、<br>
-                創作を楽しむきっかけを提供します
+                <!-- 必要なのは「作ってみたい」という好奇心だけ。<br>
+                今こそクリエイティブを体験しよう。<br> -->
+                画像や音楽を「選んで配置する」だけ。<br>
+                1分でできるコンテンツ制作。<br>
             </p>
         </div>
 
-        <!-- {{-- 検索ウィンドウとマイページへのリンク --}} -->
-        <!-- {{-- <div class="search-wrapper"> --}}
-            {{-- 検索フォーム --}}
-            {{-- <div class="search-form-wrapper">
-                <form method="POST" class="search-form" action="/media/show/search/result">
-                    @csrf
-                    <input class="search-input" type="text" name="keyword" size="30" placeholder="検索ワード">
-                    <button type="submit" class="search-icon-wrapper">
-                        <i class="fas fa-search search-icon"></i>
-                    </button>
-                </form>
-            </div>
-            <p class="sample-keywords">カフェ、雨、勉強、作業用、ロック、自然、chill、etc...</p> --}}
-        {{-- </div> --}} -->
         <!-- {{-- マイページへのリンク --}} -->
-        <div class="link-wrapper">
-            <span class="link-message">自分のメディアを作成する。</span>
+        <div class="link-wrapper" v-if="getIsLogin">
             <a class="link-to-mypage" href="/mypage">マイページへ</a>
-            <!-- {{-- <button class="link-to-mypage-button"></button> --}} -->
+        </div>
+        <!-- ゲストログイン＆コンテンツ作成画面へ移動 -->
+        <div class="link-wrapper" v-if="!getIsLogin">
+            <span class="link-message">試しにコンテンツを作ってみる</span>
+            <a class="link-to-mypage" href="/login/guest">ゲストログイン＆コンテンツ作成</a>
         </div>
     </div>
 
@@ -59,11 +49,76 @@ export default {
     computed :{
         ...mapGetters('loginState', ['getIsLogin']),
         ...mapGetters('loginState', ['getIsGuest']),
+        message_1:function(){
+            return getIsLogin ? "メディアを作る" : "ログインして自分のメディアを作る";
+        },
+        link_1_mgs:function(){
+            return getIsLogin ? "" : "";
+        },
+        message_2:function(){
+            // return getIsLogin ? "ゲストとしてメディアを作ってみる" : "ゲストとしてメディアを作ってみる";
+            return getIsLogin ? "ゲストとしてメディアを作ってみる" : "ゲストとしてメディアを作ってみる";
+        },
+
     },
 }
 
 </script>
 
 <style scoped>
+
+/* トップ */
+.top-message-wrapper {
+  position: absolute;
+  top: 10%;
+  color: white;
+  text-align: center;
+  text-shadow: 2px 2px 5px grey;
+}
+
+.top-message {
+  font-size: 50px;
+  font-family: "Yu Mincho Medium";
+  /* text-align: center; */
+  margin-bottom: 0;
+}
+
+.sub-catch-copy {
+    font-family: serif;
+}
+
+.sub-message {
+    margin-top : 200px;
+}
+
+
+/* スマホ以上のサイズ(PC、タブレット */
+@media screen and (min-width: 481px) {
+  .top-message-wrapper {
+    left: 10%;
+  }
+}
+
+/* タブレットサイズ */
+@media screen and (max-width: 780px) {}
+
+/* スマホサイズ */
+@media screen and (max-width: 480px) {
+  .top-message-wrapper {
+    /* max-width: 400px; */
+    width: 90%;
+    font-size: 0.8rem;
+    text-align: center;
+  }
+
+  .top-message {
+    font-size: 1.5rem;
+  }
+  .link-wrapper {
+    /* 縦積みに */
+    flex-direction: column;
+  }
+
+}
 
 </style>
