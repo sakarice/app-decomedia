50ca9a089409162d5434645c2af4a223df6c2e43
sakarice <41548156+sakarice@users.noreply.github.com>
2022-02-28 14:54:18 +0900

    コピー元と先の内容が逆になっていたので修正（ぼんみす・・・）

--- config/aws_cloud_front.php
+++ config/aws_cloud_front.php
@@ -4,11 +4,12 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Audio Thumbnail Url
+    | cloudFront Info
     |--------------------------------------------------------------------------
     |
-    | This value is the url of default audio thumbnail (=S3 file url)
+    | This value is the distribution of default cloudFront
     */
 
-    'url' =>"https://".config('app.aws_bucket').".s3.".config('app.aws_default_region').".amazonaws.com/public/img/audio_thumbnail/default/8%E5%88%86%E9%9F%B3%E7%AC%A6%E3%81%AE%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B3%E7%B4%A0%E6%9D%90+2.png",
+    'distribution' => 'https://d32mvq5x1dj8b7.cloudfront.net',
+
 ];
