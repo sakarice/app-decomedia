5312812ff3d26ec233e265e15dfd23bfd0eccfb9
sakarice <41548156+sakarice@users.noreply.github.com>
2022-03-07 22:47:12 +0900

    【新規製造】数値設定パーツのセットをコンポーネント化

---
+++ resources/js/components/media/edit_parts/NumSettingTemplateComponent.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="setting w100 flex j-s-between a-end">
+    <h3 class="sub-title mb0">{{subTitle}}</h3>
+    <div class="flex a-center">
+      <i class="fas fa-minus fa-lg btns minus-btn mr10" @click.stop="minusOneValue()"></i>
+      <i class="fas fa-plus fa-lg btns plus-btn ml10" @click.stop="plusOneValue()"></i>
+    </div>
+    <input type="number" class="w60px" :value="numValue" @input="updateValue($event.target.value)">
+  </div>
+</template>
+
+<script>
+
+export default {
+  props : [
+    'subTitle',
+    'numValue'
+  ],
+  data : () => {
+    return {
+    }
+  },
+  methods : {
+    minusOneValue(){
+      this.$emit('minusOneValue');
+    },
+    plusOneValue(){
+      this.$emit('plusOneValue');
+    },
+    updateValue(value){
+      this.$emit('updateValue', value);
+    }
+  },
+
+}
+</script>
+
+
+<style scoped>
+
+@import "/resources/css/mediaEditModals.css";
+@import "/resources/css/flexSetting.css";
+@import "/resources/css/FrequentlyUseStyle.css";
+
+
+</style>
